9e412c5dfd4153d5e9d7922b1da709e686068f41
John <john.e.lindqvist@proton.me>
2026-09-06 12:30:44 +0200

    Add EF Core migration for RecipeVersion Warnings column

--- src/MiseRecipeExtractor.Infrastructure/Migrations/20260906100010_AddWarningsToRecipeVersion.Designer.cs
+++ src/MiseRecipeExtractor.Infrastructure/Migrations/20260906100010_AddWarningsToRecipeVersion.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiseRecipeExtractor.Infrastructure;
 
@@ -11,9 +12,11 @@ using MiseRecipeExtractor.Infrastructure;
 namespace MiseRecipeExtractor.Infrastructure.Migrations
 {
     [DbContext(typeof(RecipeDbContext))]
-    partial class RecipeDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260906100010_AddWarningsToRecipeVersion")]
+    partial class AddWarningsToRecipeVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.10");
---
+++ src/MiseRecipeExtractor.Infrastructure/Migrations/20260906100010_AddWarningsToRecipeVersion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MiseRecipeExtractor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWarningsToRecipeVersion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Warnings",
+                table: "RecipeVersions",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Warnings",
+                table: "RecipeVersions");
+        }
+    }
+}
